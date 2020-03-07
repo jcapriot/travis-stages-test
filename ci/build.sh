6c0335871da6ad48e6be3a9345de9c8e1726c3cb
Joseph Capriotti <jcapriot@mines.edu>
2020-03-07 12:00:51 -0700

    do not error because minconda is already a cache

--- ci/build.sh
+++ ci/build.sh
@@ -3,7 +3,7 @@
 # Setup anaconda
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
 chmod +x miniconda.sh
-./miniconda.sh -b -p $HOME/miniconda
+./miniconda.sh -b -f -p $HOME/miniconda
 export PATH=/home/travis/anaconda/bin:/home/travis/miniconda/bin:$PATH
 conda update --yes conda
 
